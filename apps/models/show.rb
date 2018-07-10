4e9acdbf4da955a29ea8b928eef64a28b152bf6b
Vishal Sharma <vsh1819@gmail.com>
2018-07-10 12:48:36 +0000

    Done.

---
+++ apps/models/show.rb
@@ -0,0 +1,33 @@
+class Show < ActiveRecord::Base
+  def self.highest_rating
+    self.maximum(:rating)
+  end
+  
+  def self.most_popular_show
+    self.where("rating = ?",self.highest_rating).first
+  end
+  
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+  
+  def self.least_popular_show
+    self.where("rating = ?",self.lowest_rating).first
+  end
+  
+  def ratings_sum
+    self.sum(:rating)
+  end
+  
+  def popular_shows
+    self.where("rating > ?", 5)
+  end
+  
+  def shows_by_alphabetical_order
+    self.order(:name)
+  end
+  
+  
+  
+   
+end
